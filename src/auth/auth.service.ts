4eb23a63ef259f653d11553f59da9161a89796be
Henrique Marques <hmdev132@gmail.com>
2024-01-13 19:13:55 -0300

    fix: remove bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,86 +1,90 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { UserAuth } from './user-auth.entity';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { UserAuth } from './user-auth.entity'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  constructor(
+  constructor (
     @InjectRepository(UserAuth)
     private readonly userRepository: Repository<UserAuth>,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<UserAuth | null> {
-    const user = await this.userRepository.findOne({ where: { username } });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      return user;
+  async validateUser (
+    username: string,
+    password: string,
+  ): Promise<UserAuth | null> {
+    const user = await this.userRepository.findOne({ where: { username } })
+    if (user && user.password === password) {
+      return user
     }
-    return null;
+    return null
   }
 
-  async createUser(data: {
-    username: string;
-    password: string;
-    role: string;
-    nome?: string;
-    cpf?: string;
-    dataNascimento?: string;
-    email?: string;
-    telefone?: string;
-    estado?: string;
-    regra?: string;
-    plano?: string;
-    planoStart?: Date;
-    planoFinish?: Date;
-    pdv?: string;
-    parceiro?: string;
+  async createUser (data: {
+    username: string
+    password: string
+    role: string
+    nome?: string
+    cpf?: string
+    dataNascimento?: string
+    email?: string
+    telefone?: string
+    estado?: string
+    regra?: string
+    plano?: string
+    planoStart?: Date
+    planoFinish?: Date
+    pdv?: string
+    parceiro?: string
   }): Promise<UserAuth> {
-    const { username, password, role, ...rest } = data;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = this.userRepository.create({ username, password: hashedPassword, role, ...rest });
-    return await this.userRepository.save(newUser);
+    const { password, ...rest } = data
+    const newUser = this.userRepository.create({ password, ...rest })
+    return await this.userRepository.save(newUser)
   }
 
-  async getUserById(id: number): Promise<UserAuth | null> {
-    return this.userRepository.findOne({ where: { id } });
+  async getUserById (id: number): Promise<UserAuth | null> {
+    return this.userRepository.findOne({ where: { id } })
   }
 
-  async getUsersByRole(role: string): Promise<UserAuth[]> {
-    return this.userRepository.find({ where: { role } });
+  async getUsersByRole (role: string): Promise<UserAuth[]> {
+    return this.userRepository.find({ where: { role } })
   }
 
-  async updateUser(id: number, data: {
-    username?: string;
-    password?: string;
-    role?: string;
-    nome?: string;
-    cpf?: string;
-    dataNascimento?: string;
-    email?: string;
-    telefone?: string;
-    estado?: string;
-    regra?: string;
-    plano?: string;
-    planoStart?: Date;
-    planoFinish?: Date;
-    pdv?: string;
-    parceiro?: string;
-  }): Promise<UserAuth | null> {
-    const user = await this.userRepository.findOne({ where: { id } });
+  async updateUser (
+    id: number,
+    data: {
+      username?: string
+      password?: string
+      role?: string
+      nome?: string
+      cpf?: string
+      dataNascimento?: string
+      email?: string
+      telefone?: string
+      estado?: string
+      regra?: string
+      plano?: string
+      planoStart?: Date
+      planoFinish?: Date
+      pdv?: string
+      parceiro?: string
+    },
+  ): Promise<UserAuth | null> {
+    const user = await this.userRepository.findOne({ where: { id } })
     if (user) {
-      const { password, ...rest } = data;
+      const { password, ...rest } = data
       if (password) {
-        user.password = await bcrypt.hash(password, 10);
+        user.password = await bcrypt.hash(password, 10)
       }
-      Object.assign(user, rest);
-      return this.userRepository.save(user);
+      Object.assign(user, rest)
+      return this.userRepository.save(user)
     }
-    return null;
+    return null
   }
 
-  async deleteUser(id: number): Promise<void> {
-    await this.userRepository.delete(id);
+  async deleteUser (id: number): Promise<void> {
+    await this.userRepository.delete(id)
   }
 }
-
